3468fb6e56c3f5c49dd35a2d9c4bb43cc4224cdf
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-06-08 04:24:06 -0500

    Install modules and ann .env

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1da1f2',
+        secondary: '#14171a',
+      },
+      fontFamily: {
+        body: ['Nunito', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };

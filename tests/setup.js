3beecbf96be2f129627b9d6a8b89025ae5e92e59
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-06-13 05:35:23 -0500

    Add better error handling

--- tests/setup.js
+++ tests/setup.js
@@ -8,3 +8,20 @@ expect.extend(matchers);
 afterEach(() => {
   cleanup();
 });
+
+// Global error handler for unhandled promise rejections
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled Rejection:', reason);
+  // Optionally, you can throw an error to make it fail the test
+  throw reason;
+});
+
+// Mock console.error to suppress error output in tests
+beforeAll(() => {
+  vi.spyOn(console, 'error').mockImplementation(() => {});
+});
+
+// Restore console.error after all tests
+afterAll(() => {
+  console.error.mockRestore();
+});
